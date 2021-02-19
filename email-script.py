b53fb7712137eb6c38b087e8c5eda2b7ea777d47
Luis Villamizar <luis.villamizar@gmail.com>
2021-02-18 22:16:55 -0300

    started working on send mail feature

--- email-script.py
+++ email-script.py
@@ -1,8 +1,30 @@
 import pyperclip
+import win32com.client as client
 
-# CR
+# Variables:
+outlook = client.Dispatch('Outlook.Application')
+message = outlook.CreateItem(0)
+
 print("Please insert your CR:")
 CR = input()
+
+# Functions
+def mail(w, x, y, z):
+    message.To = w
+    message.CC = x
+    message.Subject = y
+    message.Body = z 
+
+
+# Recipients mails
+recipients = {
+    "elk": "aip.elk@accenture.com",
+    "cp": "aip.engineering.global@accenture.com;PDC.IS.AIP.MSS.SOC@accenture.com;AIP.L2@accenture.com;AIP.L2.Region_Leads@accenture.com",
+    "enablement": "AIP_Enablement@accenture.com",
+    "vault": "AIP.Cloud.Key.Management.team@accenture.com",
+    "qualys": "aip.security@accenture.com;AIP_NESSUS@accenture.com",
+    "patching": "aip.patchmgt.global@accenture.com"
+}
 
 start_menu="Please select an email option:\n \
     a-Deployment. \n \
@@ -12,10 +34,10 @@ Deployment_menu = "Please select an option:\n \
     a-Communication Plan For newly launched Instance/s\n \
     b-Update Patching list\n \
     c-ELK Signoff"
-deplo_options = {
-  "a": str("Hi Team,\n\nBelow instance has been provisioned for H365 Dev as part of "+CR+":"),
-  "b": str("Hi Team,\n\nBelow instance has been provisioned for Sun Chemicals Prod as part of "+CR+", please add it to patching list."),
-  "c": "Hi elk team,\n\nCan you please check if logs are reaching to Kibana from this instance:"
+subjects = {
+    "a": str("Hi Team,\n\nBelow instance has been provisioned for H365 Dev as part of "+CR+":"),
+    "b": str("Hi Team,\n\nBelow instance has been provisioned for Sun Chemicals Prod as part of "+CR+", please add it to patching list."),
+    "c": "Hi elk team,\n\nCan you please check if logs are reaching to Kibana from this instance:"
 }
 Decommission_menu = "Please select an option:\n \
     a-Communication Plan For newly decommisioned Instance/s\n \
@@ -38,11 +60,11 @@ if start_option == "a":
     print(Deployment_menu)
     dep_option = input()
     if dep_option == "a":
-        pyperclip.copy(deplo_options["a"])
+        pyperclip.copy(subjects["a"])
     elif dep_option == "b":
-        pyperclip.copy(deplo_options["b"])
+        pyperclip.copy(subjects["b"])
     elif dep_option == "c":
-        pyperclip.copy(deplo_options["c"])
+        pyperclip.copy(subjects["c"])
     else: 
         print("Invalid option.\n", Deployment_menu)
 # Decommission mails
@@ -53,12 +75,14 @@ elif start_option == "b":
         pyperclip.copy(dec_options["a"])
     elif dec_option == "b":
         pyperclip.copy(dec_options["b"])
-    elif dep_option == "c":
+    elif dec_option == "c":
         pyperclip.copy(dec_options["c"])
-    elif dep_option == "d":
+    elif dec_option == "d":
         pyperclip.copy(dec_options["d"])
     else:
         print("Invalid option.\n", Decommission_menu)
 else:
     print("Invalid option", start_menu)
 
+ # 0 is the code for a mail item (see the enumerations)
+message.Display()
